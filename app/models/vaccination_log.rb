28bcd812ea5ef345893bd1cfa464f4848449c6d1
akihitot <atokimatu2@gmail.com>
2021-09-17 10:49:07 +0900

    検索機能修正1

--- app/models/vaccination_log.rb
+++ app/models/vaccination_log.rb
@@ -1,11 +1,12 @@
 class VaccinationLog < ApplicationRecord
   belongs_to :medical_user
-  # has_one :user
+
   def self.search(search)
     if search != ""
-      VaccinationLog.where('my_number LIKE(?)', "%#{search}%")
+      VaccinationLog.where(["my_number LIKE ? OR manufacturer LIKE ? OR lot_number LIKE ? OR vaccination_times LIKE ? ", "#{search}", "#{search}", "#{search}", "#{search}"])
     else
       VaccinationLog.all
     end
   end
+  
 end
